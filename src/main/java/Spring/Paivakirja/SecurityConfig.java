7ddd8bc850fe6e0dc84f27fe6329c5e56977faa0
OpiskelijaMiika <miika.kangas@seamk.fi>
2023-12-09 20:59:20 +0200

    Käyttäjän luonti toimii

--- src/main/java/Spring/Paivakirja/SecurityConfig.java
+++ src/main/java/Spring/Paivakirja/SecurityConfig.java
@@ -29,7 +29,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
             .authorizeRequests()
-                .antMatchers("/", "/lisaa").permitAll()
+                .antMatchers("/", "/lisaa", "/lista", "/register").permitAll()
                 .anyRequest().authenticated()
                 .and()
             .formLogin()
